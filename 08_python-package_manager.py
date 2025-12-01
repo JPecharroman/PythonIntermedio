904cf628718f7f8000431c91d5ba888cc12b96be
JorgePC <jorge.pecharroman@gmail.com>
2025-12-01 20:19:21 +0100

    feat: Add file explaining Python's package manager (pip) and its functionalities.

---
+++ 08_python-package_manager.py
@@ -0,0 +1,14 @@
+# Gestor de paquetes de Python
+# El gestor de paquetes de Python es pip
+# pip se usa para instalar, actualizar y desinstalar paquetes de Python
+# pip se usa para buscar paquetes de Python
+# pip se usa para listar paquetes de Python
+# pip se usa para mostrar informacion de un paquete de Python
+# pip se usa para desinstalar paquetes de Python
+# pip se usa para actualizar paquetes de Python
+# pip se usa para instalar paquetes de Python desde un archivo requirements.txt
+# pip se usa para desinstalar paquetes de Python desde un archivo requirements.txt
+# pip se usa para actualizar paquetes de Python desde un archivo requirements.txt
+# pip se usa para mostrar informacion de un paquete de Python desde un archivo requirements.txt
+# pip se usa para listar paquetes de Python desde un archivo requirements.txt
+# pip se usa para buscar paquetes de Python desde un archivo requirements.txt
