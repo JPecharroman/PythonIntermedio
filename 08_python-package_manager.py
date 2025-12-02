c206834417bfa89af53b99d162bab980474af0e1
JorgePC <jorge.pecharroman@gmail.com>
2025-12-02 19:51:29 +0100

    feat: Add PyPI link, pip version check commands, and a package installation example.

--- 08_python-package_manager.py
+++ 08_python-package_manager.py
@@ -1,5 +1,7 @@
 # Gestor de paquetes de Python
 # El gestor de paquetes de Python es pip
+# Web de PIP https://pypi.org/
+
 # pip se usa para instalar, actualizar y desinstalar paquetes de Python
 # pip se usa para buscar paquetes de Python
 # pip se usa para listar paquetes de Python
@@ -12,3 +14,12 @@
 # pip se usa para mostrar informacion de un paquete de Python desde un archivo requirements.txt
 # pip se usa para listar paquetes de Python desde un archivo requirements.txt
 # pip se usa para buscar paquetes de Python desde un archivo requirements.txt
+
+# Vamos a comprobar si pip esta instalado
+pip --version
+# Si no tenemos pip en el PATH de windows, para ver la version debemos colocar antes python -m
+python -m pip --version
+# Para añadir pip en el PATH de windows 
+
+# Vamos a instalar el paquete requests
+pip install requests
